90cb035724b91540622b4566ab24cdc21b424a64
Md Mahamodul Islam <mahamodul.shakil11@gmail.com>
2024-10-02 22:50:45 +0200

    plusone

--- leetcode_top_150/66_plus1.py
+++ leetcode_top_150/66_plus1.py
@@ -1,15 +1,22 @@
 from typing import List
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        sum = ""
-        num = 0
-        output =[]
-        for i in range(len(digits)):
-            sum = sum + str(digits[i])
-        num = str(int(sum) + 1)
-        for j in range(len((num))):
-            output.append(int(num[j]))
-        return output
+        # sum = ""
+        # num = 0
+        # output =[]
+        # for i in range(len(digits)):
+        #     sum = sum + str(digits[i])
+        # num = str(int(sum) + 1)
+        # for j in range(len((num))):
+        #     output.append(int(num[j]))
+        # return output
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            else:
+                digits[i] = 0
+        return [1] + digits
 
 
 
