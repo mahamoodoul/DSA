5c8dc884b55e58d0ebd5b7a054d7671cf1ff5208
Md Mahamodul Islam <mahamodul.shakil11@gmail.com>
2024-10-03 23:30:15 +0200

    single number

---
+++ leetcode_top_150/136_single_number.py
@@ -0,0 +1,14 @@
+from typing import List
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        result = 0
+        for num in nums:
+            result ^= num 
+        return result
+            
+
+
+obj = Solution()
+nums = [4,1,2,1,2]
+output = obj.singleNumber(nums)
+print(output)
