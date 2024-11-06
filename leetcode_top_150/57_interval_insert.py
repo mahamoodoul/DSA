9828481debca2eed7d2caa85c281da3b2ddac126
Md Mahamodul Islam <mahamodul.shakil11@gmail.com>
2024-11-06 13:41:07 +0100

    insert interval

---
+++ leetcode_top_150/57_interval_insert.py
@@ -0,0 +1,31 @@
+from typing import List
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        result = []
+        n = len(intervals)
+        i = 0
+        while i < n and intervals[i][1] < newInterval[0]:
+            result.append(intervals[i])
+            i+= 1
+        while i < n and intervals[i][0] <= newInterval[1]:
+            newInterval[0] = min(newInterval[0],intervals[i][0])
+            newInterval[1] = max(newInterval[1],intervals[i][1])
+            i+=1
+        result.append(newInterval)
+
+        while i < n:
+            result.append(intervals[i])
+            i+=1
+        return result
+
+
+
+
+
+
+obj = Solution()
+intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]]
+newInterval = [4,8]
+output = obj.insert(intervals, newInterval)
+print(output)
+
