174e6bc3a09f3acdb3bbd99b88fe6e99e04bc8a2
Md Mahamodul Islam <mahamodul.shakil11@gmail.com>
2024-11-11 11:19:29 +0100

    stack implementation

---
+++ leetcode_top_150/155_min_stack.py
@@ -0,0 +1,41 @@
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+        
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+        
+
+    def pop(self) -> None:
+        if self.stack:
+            val = self.stack.pop()
+            if self.min_stack and val == self.min_stack[-1]:
+                self.min_stack.pop()
+        
+
+    def top(self) -> int:
+        if self.stack:
+            return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+        if self.min_stack:
+            return self.min_stack[-1]
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+min_stack = MinStack()
+min_stack.push(-2)
+min_stack.push(0)
+min_stack.push(-3)
+print(min_stack.getMin())  # Output: -3
+min_stack.pop()
+print(min_stack.top())     # Output: 0
+print(min_stack.getMin())  # Output: -2
